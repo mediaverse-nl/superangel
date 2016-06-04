47918c3f6f925b01b9610fc11e640f1814e9e7de
Stefan <stefanfransen1@hotmail.com>
2016-06-04 23:57:22 +0200

    Migrate fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\ItemCategories;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,11 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $data = ItemCategories::where('group', null)->get();
-        foreach ($data as $object) {
-            $object->subcategories = ItemCategories::where('group', $object->name)->get();
-        }
-        View::share('categories', $data);
     }
 
     /**
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use App\ItemCategories;
+use Illuminate\Support\Facades\View;
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
@@ -25,7 +28,13 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(Router $router)
     {
         //
-
+        if (defined('BASEPATH')){
+            $data = ItemCategories::where('group', null)->get();
+            foreach ($data as $object) {
+                $object->subcategories = ItemCategories::where('group', $object->name)->get();
+            }
+            View::share('categories', $data);
+        }
         parent::boot($router);
     }
 
